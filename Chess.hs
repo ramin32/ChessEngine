b94e6a873e32608bc2a4093c89ca0b008df743db
ramin <ramin@laptop.localdomain>
2009-05-25 10:52:50 -0400

    Refactored ChessPiece to use record syntax. Added pieceAt function.

--- Chess.hs
+++ Chess.hs
@@ -7,7 +7,10 @@ data PieceName = Pawn | Knight | Bishop | Rook | Queen | King deriving (Eq, Ord,
 
 data Color = White | Black deriving (Eq, Show)
 
-type ChessPiece = (PieceName, Color, Position) 
+data ChessPiece = ChessPiece { name :: PieceName
+                             , color :: Color
+                             , position :: Position}
+                  deriving (Eq, Show)
 
 type GameSetup = [ChessPiece]
 
@@ -15,18 +18,25 @@ positionsByRank :: Int -> [Position]
 positionsByRank rank = zip ['a'..'h'] (cycle [rank]) 
 
 pawns :: Color -> Int -> GameSetup
-pawns color rank  = [(Pawn, color, pos) | pos <- positionsByRank rank]
+pawns color rank  = [ChessPiece {name = Pawn, color = color, position = pos} | pos <- positionsByRank rank]
 
 whitePawns = pawns White 2
 blackPawns = pawns Black 7
 
 otherPieces :: Color -> Int -> GameSetup
-otherPieces color rank = zip3 [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook] 
-                              (cycle [color]) 
-                              (positionsByRank rank)
+otherPieces color rank = [ChessPiece {name = n, color = c, position = p} 
+                         | (n, c, p) <- zip3 [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook] 
+                                            (cycle [color]) 
+                                            (positionsByRank rank)]
 
 whiteOtherPieces = otherPieces White 1
 blackOtherPieces = otherPieces Black 8
 
 newGameSetup :: GameSetup
 newGameSetup = concat [whitePawns, whiteOtherPieces, blackPawns, blackOtherPieces]
+
+pieceAt :: GameSetup -> Position -> Maybe ChessPiece
+pieceAt [] pos = Nothing
+pieceAt (x:xs) pos
+    | (position x) == pos = Just x
+    | otherwise = pieceAt xs pos
